606d72982f66bb9391e49621caf4f37215aa4e73
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-06 21:14:05 -0300

    chore(tailwind): add theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,22 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./components/**/*.tsx", "./app/**/*.tsx"],
-  theme: {},
+  theme: {
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
+    colors: {
+      moderateViolet: "hsl(263, 55%, 52%)",
+      veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+      veryDarkBlackishBlue: "hsl(219, 29%, 14%)",
+      white: "hsl(0, 0%, 100%)",
+      lightGray: "hsl(0, 0%, 81%)",
+      lightGrayishBlue: "hsl(210, 46%, 95%)",
+    },
+    backgroundImage: {
+      quotation: "url('/bg-pattern-quotation.svg')",
+    },
+  },
 };
 export default config;
