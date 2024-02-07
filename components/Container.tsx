71278cfecff041196a8b3566306a425f273af5da
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-06 21:14:49 -0300

    feat: add container component

---
+++ components/Container.tsx
@@ -0,0 +1,8 @@
+type ContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Container({ children, className }: ContainerProps) {
+  return <div className={`${className}`}>{children}</div>;
+}
