5dfdf9f19a63e70257e7425a45a47ed02c5d5795
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-07 06:40:05 -0300

    style: add styles to title component

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,7 +1,8 @@
 type TitleProps = {
   children: string;
+  className?: string;
 };
 
-export default function Title({ children }: TitleProps) {
-  return <p>{children}</p>;
+export default function Title({ children, className }: TitleProps) {
+  return <p className={`${className} text-lg font-semibold`}>{children}</p>;
 }
