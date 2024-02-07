fc6d2ece7c8082f3f34703c41e68100b0e858436
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-06 21:14:30 -0300

    feat(layout): add font config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import type { NextFont } from "next/dist/compiled/@next/font";
+import { Barlow_Semi_Condensed } from "next/font/google";
 import "./globals.css";
+
+const barlowSemiCondensed: NextFont = Barlow_Semi_Condensed({
+  weight: ["500", "600"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Testimonials Grid Section",
@@ -12,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${barlowSemiCondensed.className}`}>{children}</body>
     </html>
   );
 }
